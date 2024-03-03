48b8c40b4fa45afc7f93222bf0efb467fe8ab70b
ידידיה <yed5759@gmail.com>
2024-03-03 14:17:08 +0200

    commit posts fixed

--- models/posts.js
+++ models/posts.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const postSchema = new Schema ({
-    username: String,
-    text: String,
-    img: String,
-    profilePic: String,
+    username: {
+        String,
+        required: true
+    },
+    text:{
+        String,
+        required: true
+    },
+    img:{
+        String,
+        required: true
+    },
+    profilePic:{
+        String,
+        required: true
+    },
     comments: [{user: String, body: String}]
 })
 module.exports = mongoose.model('Post', postSchema)
